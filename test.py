e6c30a2afeeb7079dc6ccb4d2cbfacd8b960fcf6
Becquet <vbecquet186@headmind.com>
2022-01-04 16:13:18 +0100

    save model in test.py torch.save

--- test.py
+++ test.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
         visuals = model.get_current_visuals()  # get image results
         img_path = model.get_image_paths()     # get image paths
         if i % 5 == 0:  # save images to an HTML file
-            model.save('model.pickle')
+            torch.save(model, 'model.pickle')
             print('processing (%04d)-th image... %s' % (i, img_path))
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize, use_wandb=opt.use_wandb)
     webpage.save()  # save the HTML
